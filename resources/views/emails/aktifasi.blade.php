996489962fa76b9b2e2609b8d976916bf31da48f
Erawan Mukti <airexhck@gmail.com>
2019-01-02 12:44:42 +0700

    perbaikan template email aktivasi akun

--- resources/views/emails/aktifasi.blade.php
+++ resources/views/emails/aktifasi.blade.php
@@ -8,7 +8,7 @@
 			Kepada Sdr/i {{$user['name']}}, Terima Kasih Sudah Mendaftar Di Website Kami.
 		</p>
 		<p>
-			Silahkan klik link <a href="{{ url('/aktifasi', ['verification_code' => $user['verification_code'], 'email' => $user['email']]) }}"> berikut</a> untuk mengaktifkan akun anda.
+			Silahkan klik link <a href="{{ url('/aktifasi?verification_code=' . $user['verification_code'] . '&email=' . $user['email']) }}"> berikut</a> untuk mengaktifkan akun anda.
 		</p>
 	</body> 
 </html>
